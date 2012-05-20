221d057ca6389f0dfb61c2d13a98533d914732bc
Jeff Bigler <jbigler@saturnstudio.com>
2012-05-20 23:33:31 +0700

    Fix typo

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -4,7 +4,7 @@
 require 'i18n/backend/active_record'
 #I18n.backend = I18n::Backend::ActiveRecord.new
 
-I18n.backend, I18n::Backend::Simple.new
+I18n.backend = I18n::Backend::Simple.new
 
 #I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Memoize)
 #I18n::Backend::ActiveRecord.send(:include, I18n::Backend::Flatten)
@@ -12,4 +12,4 @@ I18n::Backend::Simple.send(:include, I18n::Backend::Memoize)
 I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
 
 #I18n.backend = I18n::Backend::Chain.new( I18n.backend, I18n::Backend::Simple.new)
-I18n.backend = I18n::Backend::Chain.new(I18n::Backend::Simple.new, I18n.backend)
+#I18n.backend = I18n::Backend::Chain.new(I18n::Backend::Simple.new, I18n.backend)
